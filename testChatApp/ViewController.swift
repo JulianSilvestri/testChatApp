b9ef499989b163769287af01a49d2da6ce37f8df
Julian Silvestri <jsilvestri@compugen.com>
2022-02-03 10:18:26 -0500

    Initial Commit

---
+++ testChatApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  testChatApp
+//
+//  Created by Julian Silvestri on 2022-02-03.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
